6d0c3c0b76755a43e5761bf07540df3df0ab324c
Dinuka Ekanayake <dinukaekanayake92@gmail.com>
2024-06-16 23:12:42 +0800

    init security 6

---
+++ src/test/java/com/alibou/security/SpringBootSecurity6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurity6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
